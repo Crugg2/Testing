cfff80adbd0f8b0734e61e50d7e596a63e3ac763
Crugg2 <114238922+Crugg2@users.noreply.github.com>
2022-10-18 10:17:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+var x=[]
+var y=[]
+var snake
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(511, 511);
+  for (let n=0; n < 15; n++){
+    x.push(1+(51*(n)))
+    y.push(1+(51*(n)))
+  }
+  snake = new Sprite(0,0, 50, 50, 5, "E")
 }
 
 function draw() {
-  background(0,0,255);
+  background(0);
+  fill(50)
+  for (let n=0; n < 15; n++){
+    for (let s=0; s < 15; s++){
+      square(x[n],y[s],50)
+    }
+  }
+  
+  snake.update()
+  snake.draw()
 }
